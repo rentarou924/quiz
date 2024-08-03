98eea30fd287e62731c2bd652d751fc16c5804a0
rentarou924 <tunorentarou0924@gmail.com>
2024-08-03 16:53:28 +0900

    Add files via upload

---
+++ 超難問クイズ javascript.js
@@ -0,0 +1,11 @@
+function showAlert1() {
+alert("正解");
+}
+
+function showAlert2() {
+alert("不正解");
+}
+
+function kokugokotae() {
+	alert("一問目の答え:かんが 二問目の答え:非常に大きな違い 三問目の答え:心配する必要のないことを心配すること");
+}
